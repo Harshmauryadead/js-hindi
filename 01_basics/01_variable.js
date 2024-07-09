42baa4dcc5b6086cee20383ec0987f08791187e8
Harshmauryadead <97237507+Harshmauryadead@users.noreply.github.com>
2024-07-09 05:13:08 +0000

    data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144556
+let accountEmail="harsh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+//accountId = 2
+console.log(accountId);
+accountEmail = "hcscnf.com"
+accountPassword = "212122e2"
+accountCity = " Bengaluru"
+/*
+prefer not to use var
+because of issue in block scope anf function scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all Js code as newer version
+
+// alert("hello") we are using node js not browser
+
+console.log(3+3)
+// code readability should be good
+
+// number
+// bigint
+// string = " "
+// boolean
+// null - > standalone value
+// undefined
+//symbol->unique
+
+//object
+console.log(typeof 4)
+// type of null is object
